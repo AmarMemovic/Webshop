5c8b345a96c6c3f0bbb4e6d4fec7f94e98c3d8b1
Amar Memovic <amamem970511@student.jenseneducation.se>
2025-03-10 13:25:56 +0100

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Startar Webshop1...");
-        MainController mainController = new MainController();
-        mainController.start();
-    }
-}
